fb6d5ac8f5927a6ee957aab3d120267c4209c8c3
Jithin-janardhanan <jithinjanardhanan97@gmail.com>
2025-03-26 09:58:18 +0530

    created propfile page

--- AdminIOT/settings.py
+++ AdminIOT/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-)6f__uu#eh50=fj-1hq-4hb40ziviarfs@7!bh!vchw_o#+fs^
 
 DEBUG = True
 CORS_ALLOW_ALL_ORIGINS = True
-ALLOWED_HOSTS = ['192.168.20.7', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['192.168.20.14', 'localhost', '127.0.0.1']
 
 # Application definition
 
--- motors/views.py
+++ motors/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics, status, permissions
+# views.py
+from rest_framework import generics, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Motor, Valve
