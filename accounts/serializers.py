2655bcace0a424435de521f6f99d0b64c114c9a3
Jithin-janardhanan <jithinjanardhanan97@gmail.com>
2025-03-26 12:11:18 +0530

    bug issue

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import User
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
 
 class AdminLoginSerializer(serializers.Serializer):
     email = serializers.EmailField()
@@ -37,8 +40,7 @@ class AdminLoginSerializer(serializers.Serializer):
 
 
 
-from .models import User
-
+User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=False)
@@ -46,11 +48,8 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'email', 'password', 'first_name',
-                  'last_name', 'role',
-                #     'is_active', 'date_joined',
-                #   'phone_number', 'address'
-                  )
-        read_only_fields = ('id', 'date_joined')
+                  'last_name', 'role','phone_number','address')
+        read_only_fields = ('id',)
 
     def create(self, validated_data):
         password = validated_data.pop('password', None)
--- farm/serializers.py
+++ farm/serializers.py
@@ -67,7 +67,6 @@
 #         return farm
 
 from rest_framework import serializers
-from accounts.serializers import UserSerializer
 from .models import Farm, Motor, Valve
 from accounts.models import User
 
@@ -91,75 +90,32 @@ class ValveSerializer(serializers.ModelSerializer):
         return data
 
 class MotorSerializer(serializers.ModelSerializer):
-    valves = ValveSerializer(many=True, read_only=True)
+    valves = ValveSerializer(many=True, read_only=True)  # Ensure read_only to prevent incorrect writes
 
     class Meta:
         model = Motor
-        fields = ['id', 'motor_type', 'valve_count', 'valves']
+        fields = ['id', 'motor_type', 'valve_count', 'valves', 'farm']
 
     def validate(self, data):
         motor_type = data.get('motor_type')
         valve_count = data.get('valve_count')
-
+        
         max_valves = {
             'single_phase': 4,
             'double_phase': 6,
             'triple_phase': 10
         }
 
-        if valve_count > max_valves[motor_type]:
+        if motor_type and valve_count and valve_count > max_valves.get(motor_type, 0):
             raise serializers.ValidationError(
                 f"{motor_type} cannot have more than {max_valves[motor_type]} valves"
             )
         return data
 
 class FarmSerializer(serializers.ModelSerializer):
-    motors = MotorSerializer(many=True)
+    motors = MotorSerializer(many=True, read_only=True)
     owner = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
 
     class Meta:
         model = Farm
         fields = ['id', 'name', 'location', 'owner', 'motors']
-
-    def create(self, validated_data):
-        motors_data = validated_data.pop('motors', [])
-        farm = Farm.objects.create(**validated_data)
-
-        for motor_data in motors_data:
-            Motor.objects.create(farm=farm, **motor_data)
-            # Valves are created automatically via Motor.save()
-
-        farm.refresh_from_db()
-        return farm
-
-    def update(self, instance, validated_data):
-        # Update farm fields
-        instance.name = validated_data.get('name', instance.name)
-        instance.location = validated_data.get('location', instance.location)
-        instance.owner = validated_data.get('owner', instance.owner)
-        instance.save()
-
-        # Handle motors
-        motors_data = validated_data.get('motors', [])
-        existing_motors = {motor.id: motor for motor in instance.motors.all()}
-
-        # Update or create motors
-        for motor_data in motors_data:
-            motor_id = motor_data.get('id')
-            if motor_id and motor_id in existing_motors:
-                # Update existing motor
-                motor = existing_motors[motor_id]
-                motor.motor_type = motor_data.get('motor_type', motor.motor_type)
-                motor.valve_count = motor_data.get('valve_count', motor.valve_count)
-                motor.save()
-                del existing_motors[motor_id]
-            else:
-                # Create new motor
-                Motor.objects.create(farm=instance, **motor_data)
-
-        # Delete motors not included in the update
-        for motor in existing_motors.values():
-            motor.delete()
-
-        instance.refresh_from_db()
-        return instance
