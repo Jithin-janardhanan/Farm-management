d7d40b591280b0504816452486b943659097b1de
fahadaaaaa <fahadrahman8826@gmail.com>
2025-03-26 11:04:01 +0530

    code bug

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,7 +10,7 @@ from .views import HomeView, admin_login_page, admin_dashboard_page, UserListCre
 urlpatterns = [
 
 
-    path('api/admin/login/', AdminLoginView.as_view(), name='admin-login'),
+    path('api/admin/loogin/', AdminLoginView.as_view(), name='admin-login'),
     path('api/admin/home/', HomeView.as_view(), name='admin-home'),
     path('adminpage', admin_login_page, name='admin-login-page'),
     path('admin-dashboard/', admin_dashboard_page, name='admin-dashboard-page'),
